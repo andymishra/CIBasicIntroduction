dee3fdcf2e308595c80553a85cb900c69d71b73f
Anand Mishra <anand.mishra.mca@gmail.com>
2023-10-27 06:09:21 +0530

    added whole projcet

---
+++ CIBasicIntroduction/CIBasicIntroductionTests/CIBasicIntroductionTests.swift
@@ -0,0 +1,32 @@
+//
+//  CIBasicIntroductionTests.swift
+//  CIBasicIntroductionTests
+//
+//  Created by Anand Mishra on 27/10/23.
+//
+
+import XCTest
+@testable import CIBasicIntroduction
+
+class CIBasicIntroductionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+       XCTAssertEqual(2*2, 4)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
